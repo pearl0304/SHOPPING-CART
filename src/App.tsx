9453a088fd58e3887d3c0c89e393c2b864852c54
Kylie <kylie@comets.kr>
2022-07-15 15:44:42 +0900

    feat: Setting for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { Drawer, LinearProgress, Grid } from "@mui/material";
+import AddShoppingCart from "@mui/icons-material/AddShoppingCart";
+import Badge from "@mui/icons-material/Badge";
+import { Wrapper } from "./App.styles";
+import { IShopItem } from "./interfaces/shop_Items.interface";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const getProducts = async (): Promise<IShopItem[]> => await (await fetch("https://fakestoreapi.com/products")).json();
+export const App = () => {
+  const { data, isLoading, error } = useQuery<IShopItem[]>("products", getProducts);
+  if (isLoading) return <LinearProgress />;
+  return <div className="App"></div>;
+};
---
+++ src/interfaces/shop_Items.interface.ts
@@ -0,0 +1,9 @@
+export interface IShopItem {
+  id: string;
+  category: string;
+  description: string;
+  imgae: string;
+  price: number;
+  title: string;
+  amount: number;
+}
